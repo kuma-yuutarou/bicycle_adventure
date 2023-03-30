698aa35588671e127a2a9b5a78d83c6389dd03ec
kuma-yuutarou <kumatarou2294@gmail.com>
2023-03-30 13:14:38 +0000

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :notifications
+  devise_for :likes
+  devise_for :comments
+  devise_for :posts
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
