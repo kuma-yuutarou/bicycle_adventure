d1a6681677aa8616e34d7a25b158328ab0529e26
kuma-yuutarou <kumatarou2294@gmail.com>
2023-04-02 06:45:33 +0000

    sing_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
+
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource) #ログインした後のパス
+    users_path
+  end
+
+  def after_sign_out_path_for(resource) #ログアウトした後のパス
+    homes_top_path
+  end
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age, :gender, :introduction, :telephone_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age, :gender, :introduction, :telephone_number]) #新規登録したら保存されるデータのカラム　※アイコン画像imageを
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,49 @@
 Rails.application.routes.draw do
   devise_for :admins
-  devise_for :notifications
-  devise_for :likes
-  devise_for :comments
-  devise_for :posts
   devise_for :users
+
+  resources :notifications,only: [:index, :create] #onlyでだけ
+  #get 'notifications/index'
+  #get 'notifications/create'
+
+  resources :likes,only: [:create, :destroy]
+  #get 'likes/create'
+  #get 'likes/destroy'
+
+  resources :comments,only: [:create, :destroy]
+  #get 'comments/create'
+  #get 'comments/destroy'
+
+  namespace :admin do
+    resources :users,except: [:new, :create, :destroy] #except除外する書き方
+    #get 'users/index'
+    #get 'users/show'
+    #get 'users/edit'
+    #get 'users/update'
+  end
+
+  namespace :admin do
+    get 'homes/top'
+  end
+
+  resources :users,only: [:show, :edit, :update, :index] do
+    get 'unsubscribe'
+    patch 'withdrawal'
+  end
+  # get 'users/show'
+  # get 'users/edit'
+  # get 'users/update'
+
+  resources :posts #7つ含まれている　[index,show,new,create,edit,update,destroy]
+  # get 'posts/index'
+  # get 'posts/show'
+  # get 'posts/new'
+  # get 'posts/create'
+  # get 'posts/edit'
+  # get 'posts/update'
+  # get 'posts/destroy'
+
+  get 'homes/top'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
